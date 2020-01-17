2dd5cd89b221c0208f6713e14fda8cbf03f780f8
Sharon Watkins <swatkins876@gmail.com>
2020-01-17 21:15:26 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,25 @@ class StudentsController < ApplicationController
     student_path (@student)
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+
+          if @student.save
+             redirect_to student_path(@student)
+          else
+             erb :"/students/show.html"
+          end
+      end
+
   private
 
     def set_student
