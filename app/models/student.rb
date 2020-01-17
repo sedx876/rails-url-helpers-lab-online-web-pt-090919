4e0c0076fd18ec8f0b8cd24c8a657ad2e1a724f4
Sharon Watkins <swatkins876@gmail.com>
2020-01-17 21:21:28 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,18 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def is_active?
+     if self.active == true
+  "This student is currently active."
+    else
+  "This student is currently inactive."
+    end
+  end
+
+
+  def activate
+    self.active = !self.active
+    self.save
+  end
 end
